47f35e2f37d1394b6396c74adee7318ff534f8a1
tbsmcd <tbsmcd@gmail.com>
2021-02-26 23:54:56 +0900

    debug

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ from os import environ
 
 targets = [x.strip() for x in environ.get('INPUT_TARGETS').split(',')]
 print(targets)
+print(environ.get('GITHUB_EVENT_NAME'))
+print(environ.get('GITHUB_EVENT_PATH'))

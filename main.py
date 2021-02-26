f9274d577937ee5627477d2943c19672e71da9a5
tbsmcd <tbsmcd@gmail.com>
2021-02-26 23:59:26 +0900

    get event

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from os import environ
+import json
+from pprint import pprint
 
 targets = [x.strip() for x in environ.get('INPUT_TARGETS').split(',')]
 print(targets)
-print(environ.get('GITHUB_EVENT_NAME'))
-print(environ.get('GITHUB_EVENT_PATH'))
+events = json.load(environ.get('GITHUB_EVENT_PATH'))
+pprint(events)

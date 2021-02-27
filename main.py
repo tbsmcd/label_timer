50b72b889e12913b676e068464ca3c3944652848
tbsmcd <tbsmcd@gmail.com>
2021-02-28 00:00:10 +0900

    test status code

--- main.py
+++ main.py
@@ -5,14 +5,6 @@ import time
 from pprint import pprint
 import requests
 
-# with open(environ.get('GITHUB_EVENT_PATH')) as f:
-#     events = json.load(f)
-#     action = events['action']
-#     label = events['label']['name']
-#     print(action)
-#     print(label)
-#     pprint(events)
-
 
 class Label:
     def __init__(self, events, targets):
@@ -21,30 +13,34 @@ class Label:
         self.prefix = 'label_timer_' + self.events['label']['name'] + '_'
         self.headers = {'Authorization': 'token %s' % environ.get('INPUT_TOKEN')}
         self.current_labels = [x['name'] for x in self.events['issue']['labels']]
+        self.passed_seconds = 0
 
     def is_target(self):
         return self.events['label']['name'] in self.targets
 
-    def add_label(self):
+    def add(self):
         # If a label with a prefix exists, do not add a new label.
         if len([x for x in self.current_labels if x.startswith(self.prefix)]) > 0:
             return
         label_to_add = self.prefix + str(int(time.time()))
         api_url = self.events['issue']['url'] + '/labels'
         payload = {'labels': [label_to_add]}
-        requests.post(api_url, headers=self.headers, data=json.dumps(payload))
+        r = requests.post(api_url, headers=self.headers, data=json.dumps(payload))
+        print(r.status_code)
         return
 
-    def delete_label(self):
+    def remove(self):
         # List of labels with prefix
         timer_labels = [x for x in self.current_labels if x.startswith(self.prefix)]
         if len(timer_labels) == 0:
             return
         timer_labels.sort()
         start_time = int(timer_labels[0].replace(self.prefix, ''))
-        passed_seconds = time.time() - start_time
-        td = datetime.timedelta(seconds=passed_seconds)
-        print(str(td))
+        self.passed_seconds = time.time() - start_time
+        # print('::set-output name=PASSED_SECONDS::{}'.format(self.passed_seconds))
+
+    def comment(self):
+        pass
 
 
 def main():
@@ -55,9 +51,9 @@ def main():
         if label.is_target() is False:
             return
         if events['action'] == 'labeled':
-            label.add_label()
+            label.add()
         elif events['action'] == 'unlabeled':
-            label.delete_label()
+            label.remove()
 
 
 if __name__ == '__main__':

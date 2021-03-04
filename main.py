c39ee7705046a5482a932a259d45fea6915b2f6b
tbsmcd <tbsmcd@gmail.com>
2021-03-05 01:32:55 +0900

    fix comment

--- main.py
+++ main.py
@@ -50,11 +50,9 @@ class Label:
     def comment(self):
         before_passed_seconds = self.__sum_passed_seconds()
         delta = re.sub(r'\.[0-9]*$', '', str(datetime.timedelta(seconds=self.passed_seconds)))
+        total_delta = re.sub(r'\.[0-9]*$', '', str(datetime.timedelta(seconds=before_passed_seconds)))
         body = 'Label {0} passed time: {1}\n(seconds: {2})\nTotal time: {3}'.\
-            format(self.events['label']['name'],
-                   delta,
-                   int(self.passed_seconds),
-                   datetime.timedelta(seconds=before_passed_seconds + self.passed_seconds))
+            format(self.events['label']['name'], delta, int(self.passed_seconds), total_delta)
         api_url = self.events['issue']['url'] + '/comments'
         payload = {'body': body}
         r = requests.post(api_url, headers=self.headers, data=json.dumps(payload))
